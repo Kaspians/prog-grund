2d502ad219a6f71b8a92dc8634251dab637c99f7
Kaspians <kaspian-s@posteo.lu>
2026-03-27 14:39:49 +0100

    lab05 fixes, rename
    mv eureka_assets assets_eureka

--- edaa8x-labs/lab05/eureka.py
+++ edaa8x-labs/lab05/eureka.py
@@ -1,22 +1,22 @@
 #!/bin/python3
 
-from eureka_assets import fn
+from assets_eureka import fn
 
 
 def main():
     ask = fn.asking()  # who axed?
     while ask:  # as long axing ain't make no ask empty
-        if ask == "add":  # TODO: allow to write 'add "foobar"' (etc) directly
-            fn.add()
-        elif ask == "del":  # TODO: allow multiple deletes
+        if ask == 'add':
+            fn.add('')
+        elif ask == 'del':  # TODO: allow multiple deletes
             fn.ls()
             fn.delete()
-        elif ask == "ls":
+        elif ask == 'ls':
             fn.ls()
-        elif ask == "help":
+        elif ask == 'help':
             fn.help()
         else:
-            print("\nWTF is dis?", ask)
+            fn.else_what(ask)
 
         ask = fn.asking()  # I did the axing
 
@@ -25,13 +25,17 @@ def main():
 
 main()
 
+# TODO:
+# Make `ask` instead be the first part of an array,
+#   of which is the space-seperated array of user input.
+#   (so i can use the rest of the array to use as arg if != empty)
 
 ##########
 ## KRAV ##
 ##########
 #
-# - [x] Min 2 func()        - 11/2
-# - [x] Min 1 [list]        - 2/1 ? kanske way more
-# - [x] Interact with user  - 
-# - [x] Easy code to read and understand
-# - [x] keep it KISS stupid - 😘
+# - [x] Min 2 func()            - 11/2
+# - [x] Min 1 [list]            - 2/1 ? kanske way more
+# - [x] Interact with user      - 
+# - [x] Easy code to read and understand - check?
+# - [x] keep it KISS stupid     - 😘
