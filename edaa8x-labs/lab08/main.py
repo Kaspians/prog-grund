1733f34c3f30e13a1dae25bd80917dd14fbea582
Kaspians <kaspian-s@posteo.lu>
2026-04-25 16:52:04 +0200

    Better cli interface
    Now time for GUI implementation

--- edaa8x-labs/lab08/main.py
+++ edaa8x-labs/lab08/main.py
@@ -8,10 +8,9 @@ if __name__ == '__main__':
     def Main() -> None:
         """
         `Main()` run until board `is_full`
-        - 1. place
-        - 2. print board
-        - 3. restart
-        - <None>. quit
+        - [xy](int)         => `place()`
+        - 'exit'(string)    => `break`
+        - 'restart'(string) => `restart()`
 
         .. code-block:: markdown
         **markdown** like this, _btw_
@@ -21,7 +20,6 @@ if __name__ == '__main__':
         turn = 0
 
         while not t.is_full():
-            # t.print_board(False) # TEST: spammy?
             if turn % 2 == 0:
                 marker = 'X'
             else:
@@ -30,44 +28,31 @@ if __name__ == '__main__':
             ask = input(f"""
         Marker's turn: > {marker} <
 
-            What do you want to do?
-            - 1. place
-            - 2. print board
-            - 3. restart
-            - <None>. quit
+            What do you want to do? Type:
+            - [xy](int)         => `place(xy[0], xy[1])`
+            - 'exit'(string)    => `break`
+            - 'restart'(string) => `restart()`
             """)
-            if ask == '1':
-                rowcol = input('row/col? (format: [xy]) ')
-                if len(rowcol) == 2:
-                    row, col = rowcol[0], rowcol[1]
-                    if row in ('0', '1', '2') and col in ('0', '1', '2'):
-                        t.place(marker, int(row), int(col))
-                        t.print_board(False)
-                        if t.is_winner(marker):
-                            print(marker, 'WIN')
-                            break
-                        turn += 1
-                    else:
-                        print('\n    0 <= int <= 2')
+            if len(ask) == 2:
+                row, col = ask[0], ask[1]
+                if row in ('0', '1', '2') and col in ('0', '1', '2'):
+                    t.place(marker, int(row), int(col))
+                    t.print_board(False)
+                    if t.is_winner(marker):
+                        print(marker, 'WIN')
+                        break
+                    turn += 1
                 else:
-                    print('\n    only 2 int (xx)!')
-            elif ask == '2':
-                t.print_board(True)
-            elif ask == '3':
+                    print('\n    0 <= int <= 2')
+            elif ask == 'exit':
+                break
+            elif ask == 'restart':
                 t.restart()
                 t.print_board(False)
-            elif ask == '':
-                break
             else:
-                print(f"""
-                Huh?
-                Wtf is: "{ask}"
-                """)
+                print('\n\t Only 2-char-int(xy), "exit", or "restart"')
 
         t.is_full()
         print('is_full() =', t.is_full())
 
     Main()
-    # b = TicTacToeBoard()
-    # b.place('X', 0, 0)
-    # b.print_board(False)
